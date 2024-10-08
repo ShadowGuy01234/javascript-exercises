e07e5115fc4ff2043cba59c14e37bef0545b70fa
ShadowGuy01234 <121357713+ShadowGuy01234@users.noreply.github.com>
2024-10-08 23:17:19 +0530

    added more soln

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    if (Number.isInteger(n1) == true && Number.isInteger(n2) == true && n1 > 0 && n2 > 0) {
+        if (n2 > n1) {
+            for (let i = n1;i <= n2;i++) {
+                sum += i;
+            }
+        }
+        if (n1 > n2) {
+            for (let i = n2;i <= n1;i++) {
+                sum += i;
+            }
+        }
+    }
+    else {
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line

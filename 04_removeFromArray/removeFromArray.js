eb2a17d32623f183b6bc2cf64e2adf9a5f6207e1
ShadowGuy01234 <121357713+ShadowGuy01234@users.noreply.github.com>
2024-10-08 23:17:48 +0530

    added soln for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,9 @@
-const removeFromArray = function(array, n1, n2) {
-    let len = array.length
-    for (let i = 0; i < len;i++) {
-        if (array[i] == n1 || array[i] == n2) {
-            array.splice([i], 1);
-        }
-    }
-    console.log(array);
-    return array;
+function removeFromArray(array, ...args) {
+    return array.filter(x => !args.includes(x))
 };
+
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

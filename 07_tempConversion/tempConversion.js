814510494af6a1067ad1636168646907a773b726
ShadowGuy01234 <121357713+ShadowGuy01234@users.noreply.github.com>
2024-10-09 23:30:42 +0530

    completed ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg1) {
+  let cel = (deg1 - 32) * (5/9);
+  return Math.round(cel * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg2) {
+  let farh = deg2 * (9 / 5) + 32;
+  return Math.round(farh * 10)/ 10;
 };
 
 // Do not edit below this line
